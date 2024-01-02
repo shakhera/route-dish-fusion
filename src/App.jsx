6fccc1f47ffd49be1c8830b03d74244be0504298
shakhera <shakherakhanom024@gmail.com>
2024-01-03 03:58:26 +0600

    use loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 
 function App() {
+  const navigation = useNavigation();
   return (
     <>
       <Header></Header>
+      <div>{navigation.state === "loading" && "loading....."}</div>
       <Outlet></Outlet>
     </>
   );
